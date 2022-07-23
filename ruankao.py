e988f52078a90d0e9da6d7bd31449cf0d668bffa
ben_zzz <ben_zzz@163.com>
2022-07-23 22:56:43 +0800

    fix some errors

--- ruankao.py
+++ ruankao.py
@@ -10,26 +10,25 @@ import ddddocr
 import logging
 
 STAGE = '2022年上半年'
-KEY_WORD = '%s计算机技术与软件专业技术资格(水平)考试成绩查询通知' % STAGE
+KEY_WORD = '%s计算机技术与软件专业技术资格(水平)考试成绩' % STAGE
 
 CANDIDATE_NAME = '张三'  # 准考证姓名
 CANDIDATE_NO = '20228888888888888'  # 准考证号 or 身份证号
-SELECT_TYPE = '0' # 0: 准考证号 1: 身份证号
+SELECT_TYPE = '0'  # 0: 准考证号 1: 身份证号
 
 PUSH_KEY = 'xxx'  # 推送消息的key
 
 
 def init_log():
     logging.basicConfig(
-        level=logging.INFO  #设置日志输出格式
+        level=logging.INFO  # 设置日志输出格式
         ,
-        filename="/var/log/ruankao.log"  #log日志输出的文件位置和文件名
+        filename="/var/log/ruankao.log"  # log日志输出的文件位置和文件名
         ,
-        format=
-        "%(asctime)s %(filename)-8s [line: %(lineno)s] : %(message)s"  #日志输出的格式
+        # 日志输出的格式
         # -8表示占位符，让输出左对齐，输出长度都为8位
-        ,
-        datefmt="%Y-%m-%d %H:%M:%S"  #时间输出的格式
+        format="%(asctime)s %(filename)-8s [line: %(lineno)s] : %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"  # 时间输出的格式
     )
 
 
@@ -49,10 +48,10 @@ def check_result():
 
 
 def query_result():
-    response = requests.get(
+    response1 = requests.get(
         'https://query.ruankao.org.cn//score/captcha?1654599907554')
     with open('/tmp/captcha.png', 'wb') as f:
-        f.write(response.content)
+        f.write(response1.content)
 
     ocr = ddddocr.DdddOcr(show_ad=False)
     with open('/tmp/captcha.png', 'rb') as f:
@@ -65,15 +64,15 @@ def query_result():
     headers = {
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
     }
-    response = requests.post(
+    response2 = requests.post(
         'https://query.ruankao.org.cn//score/VerifyCaptcha',
         data=data,
         headers=headers,
-        cookies=response.cookies.get_dict())
+        cookies=response1.cookies.get_dict())
 
-    logging.info(response.json()["flag"])
+    logging.info(response2.json()["flag"])
 
-    if response.json()["flag"] == 1:
+    if response2.json()["flag"] == 1:
         params = {
             'stage': STAGE,
             'xm': CANDIDATE_NAME,
@@ -81,13 +80,22 @@ def query_result():
             'jym': captcha,
             'select_type': SELECT_TYPE
         }
-        response = requests.post('https://query.ruankao.org.cn//score/result',
-                                 data=params,
-                                 headers=headers,
-                                 cookies=response.cookies.get_dict())
-        logging.info(response.json()["flag"])
-        if response.json()["flag"] == 1:
-            return str(response.json()["data"])
+        response3 = requests.post('https://query.ruankao.org.cn//score/result',
+                                  data=params,
+                                  headers=headers,
+                                  cookies=response1.cookies.get_dict())
+        logging.info(response3.json()["flag"])
+        if response3.json()["flag"] == 1:
+            SWCJ = float(response3.json()["data"]["SWCJ"])
+            XWCJ = float(response3.json()["data"]["XWCJ"])
+            LWCJ = float(response3.json()["data"]["LWCJ"])
+
+            prefix = '恭喜通过考试!'
+            if SWCJ < 45 or XWCJ < 45 or LWCJ < 45:
+                prefix = '很遗憾, 请再接再厉!'
+            msg = "%s %s%s: 上午成绩%s, 下午成绩%s, 论文成绩%s" % (prefix, response3.json()["data"]["KSSJ"], response3.json()["data"]["ZGMC"], response3.json()[
+                "data"]["SWCJ"], response3.json()["data"]["XWCJ"], response3.json()["data"]["LWCJ"])
+            return msg
     return ""
 
 
@@ -106,6 +114,7 @@ def main():
     if check_result():
         logging.info("The results have been announced, please check in time")
         push_result("%s软考成绩已公布, 请及时查询" % STAGE)
+        sleep(2)
         while True:
             result = query_result()
             if result == "":
@@ -113,7 +122,7 @@ def main():
                 sleep(2)
             else:
                 logging.info("The score is: %s" % result)
-                push_result("%s软考成绩为: %s" % STAGE, result)
+                push_result("%s软考, %s" % (STAGE, result))
                 ret = 0
                 break
     return ret
